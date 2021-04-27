8ca623d19213a63d50907c1876cfde9353f621e8
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-04-27 04:46:51 +0000

    revision

--- head.js
+++ head.js
@@ -1,18 +1,18 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`)
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-      console.log(`Assertion Failed: ${actual} !== ${expected}`);
-    }
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
-function head(array) {
+const head = function(array) {
   if (array) {
     return array[0];
   } else {
     return undefined;
   }
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
