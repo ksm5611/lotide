356238f8186b0726e240bc4e0ca0aa47af227e2d
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-04-27 02:47:20 +0000

    assertion

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('Assertion Passed: ' + [actual] + ' === ' + [expected]);
+  } else if (actual !== expected) {
+    console.log('Assertion Failed: ' + [actual] + ' !== ' + [expected]);
+  }
+  
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual("apple", "apple");
+assertEqual("banana", "apple");
+assertEqual(9000, 9000);
+assertEqual(0, 123);
