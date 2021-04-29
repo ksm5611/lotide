326877314a3f51750482251fa0e691f7e04255c3
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-04-29 01:50:51 +0000

    make it correct

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
+      return false
+    }
+  }
+
+  return true;
+
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]] === undefined) {
+      results[sentence[i]] = [i]
+    } else {
+      results[sentence[i]].push(i)
+    }
+  }
+  return results
+};
+
+
+assertArraysEqual(letterPositions('hello')['h'], [0])
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions('hello')['l'], [2, 3])
+assertArraysEqual(letterPositions('hello')['o'], [4])
+
+
