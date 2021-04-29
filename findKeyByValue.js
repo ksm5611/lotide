3066e6c8c49b92802fee2a15ff9b6251624f70b9
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-04-29 02:42:01 +0000

    Object.key()

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  for (let key of Object.keys(bestTVShowsByGenre)) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
