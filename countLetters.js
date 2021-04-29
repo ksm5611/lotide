cce2610cb1879a743785100b571cd49427dcf616
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-04-29 00:25:42 +0000

    counting letters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`)
+  } else if (actual !== expected) {
+      console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    }
+};
+
+const countLetters = function(texts) {
+  let result = {};
+  for (const text of texts) {
+    if (result[text] === undefined) {
+      result[text] = 1;
+    } else {
+      result[text] += 1
+    }
+    
+  }
+  return result;  
+}
+
+
+console.log(countLetters('LHL'))
