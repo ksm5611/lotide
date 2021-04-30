ca03b59bbe81f03d8c5cb44a5828a682ce409e88
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-04-30 03:04:40 +0000

    interesting

---
+++ findKey.js
@@ -0,0 +1,31 @@
+const findKey = function(obj, callback) {
+  const scaning = Object.keys(obj).find((firstKey) => {
+    // console.log(obj[firstKey])
+    return callback(obj[firstKey]);
+  });
+  return scaning;
+};
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const result =  findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+console.log(result);
+
+
+assertEqual(result, "noma");
