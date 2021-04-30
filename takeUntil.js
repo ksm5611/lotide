a870c6ecdc25fcca8ef217d37d73b741b72fbe0b
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-04-30 01:58:59 +0000

    using findIndex and slice

--- takeUntil.js
+++ takeUntil.js
@@ -6,7 +6,27 @@ const takeUntil = function(array, callback) {
   return array.slice(0, index)
 }
 
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
 
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
+      return false
+    }
+  }
+
+  return true;
+
+};
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
@@ -18,6 +38,8 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
+assertArraysEqual([ 1, 2, 5, 7, 2 ] ,results1)
+assertArraysEqual([ 'I\'ve', 'been', 'to', 'Hollywood' ], results2)
 
 // [ 1, 2, 5, 7, 2 ]
 // ---
