2cf840858e0a8ade4869611e1717d48425b48369
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-05-04 04:45:51 +0000

    linting

--- letterPositions.js
+++ letterPositions.js
@@ -11,8 +11,8 @@ const eqArrays = function(array1, array2) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if(array1[i] !== array2[i]) {
-      return false
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
 
@@ -26,18 +26,18 @@ const letterPositions = function(sentence) {
   // logic to update results here
   for (let i = 0; i < sentence.length; i++) {
     if (results[sentence[i]] === undefined) {
-      results[sentence[i]] = [i]
+      results[sentence[i]] = [i];
     } else {
-      results[sentence[i]].push(i)
+      results[sentence[i]].push(i);
     }
   }
-  return results
+  return results;
 };
 
 
-assertArraysEqual(letterPositions('hello')['h'], [0])
+assertArraysEqual(letterPositions('hello')['h'], [0]);
 assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions('hello')['l'], [2, 3])
-assertArraysEqual(letterPositions('hello')['o'], [4])
+assertArraysEqual(letterPositions('hello')['l'], [2, 3]);
+assertArraysEqual(letterPositions('hello')['o'], [4]);
 
 
