694b9291d11edf71864488011f5f1b6f59463c07
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-05-04 01:50:02 +0000

    ahorthand syntax from Es6

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // index.js
+
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 
-module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+// module.exports = {
+//   head:   head,
+//   tail:   tail,
+//   middle: middle
+// };
+
+const allObjs = {
+  head,
+  tail,
+  middle
 };
+
+console.log(allObjs);
+
 
 // Can export a number (not common, but JS won't stop me!)
 // Can export an object (quite common!)
